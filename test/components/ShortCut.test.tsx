ecf94f003b149fcd2d774b749446fc198c620656
Willem Liu <willem_liu@hotmail.com>
2019-05-20 13:42:32 +0200

    Added test for ShortCut

---
+++ test/components/ShortCut.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import TestRenderer from 'react-test-renderer';
+import { FdEventType } from '../../src/utils/CypressDictionary';
+import ShortCut from '../../src/components/ShortCut';
+
+describe('Shortcut', () => {
+    it('should render shortcut correctly', () => {
+        let contextMenu = TestRenderer.create(<ShortCut>CTRL</ShortCut>);
+        expect(contextMenu.toJSON()).toMatchSnapshot();
+    });
+});
